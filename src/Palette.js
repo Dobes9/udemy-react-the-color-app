ff4b6e2726fa8f4ff2353698c3f768a74b0e824c
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-30 19:28:20 -0800

    style slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
 import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
 
 export default function Palette({ palette }) {
   const { paletteName, id, emoji, colors } = palette;
@@ -17,13 +17,15 @@ export default function Palette({ palette }) {
   };
   return (
     <div className="Palette">
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        onAfterChange={changeLevel}
-        step={100}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          onAfterChange={changeLevel}
+          step={100}
+        />
+      </div>
       {/* Navbar goes here */}
       <div className="Palette-colors">{colorBoxes()}</div>
       {/* footer */}
