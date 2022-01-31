3114a0d56c0dbf92360e7b87cdfc07941aa7df27
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-31 12:21:46 -0800

    defined routes from router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
+      <Routes>
+        <Route path="/" element={<h1>Palette List goes here</h1>} />
+        <Route
+          path="/palette/:paletteId"
+          element={<h1>Individual Palette</h1>}
+        />
+      </Routes>
     </div>
   );
+}
+
+{
+  /* <Palette palette={generatePalette(seedColors[4])} /> */
 }
 
 export default App;
